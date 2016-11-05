b6e532da279dbcc1f43d6b4916f67be29903ba10
DuPupu <zouluoyi@gmail.com>
2016-11-05 23:56:29 +0800

    ends

--- index.js
+++ index.js
@@ -35,29 +35,32 @@ app.get('/', function(request, response) {
 
 app.get('/getdb', function(request, response) {
 
-// Connection URL
-var url = 'mongodb://heroku_9c85x4j1:v4fqtsggd87bq3ntfbjnnopp9a@ds145667.mlab.com:45667/heroku_9c85x4j1';
-// Use connect method to connect to the server
-MongoClient.connect(url, function(err, db) {
-  assert.equal(null, err);
-  console.log("Connected correctly to server");
+    // Connection URL
+    var url = 'mongodb://heroku_9c85x4j1:v4fqtsggd87bq3ntfbjnnopp9a@ds145667.mlab.com:45667/heroku_9c85x4j1';
+    // Use connect method to connect to the server
+    MongoClient.connect(url, function(err, db) {
+      assert.equal(null, err);
+      console.log("Connected correctly to server");
 
-    findDocuments(db, function(docs) {
-      db.close();
-      response.send(docs);
-  });
-});
+        findDocuments(db, function(docs) {
+          db.close();
+          response.send(docs);
+      });
+    });
+    
 });
 
 
 app.get('/setdb', function(request, response) {
-  console.log(request.url)
+  questr = request.url.replace('/setdb?',"");
+  questr=JSON.parse('{"' + decodeURI(questr).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g,'":"') + '"}');
   co(function*() {
     // Connection URL
     var db = yield MongoClient.connect('mongodb://heroku_9c85x4j1:v4fqtsggd87bq3ntfbjnnopp9a@ds145667.mlab.com:45667/heroku_9c85x4j1');
     console.log("Connected correctly to server");
     // Insert a single document
-    var json = {name:"DKSmith",gender:"m",avCode:"m001",where:"AC1"};
+    var json = {name:questr.name.replace(/[+]/g," ")||"",gender:questr.gender||"",avCode:questr.avCode||"",where:questr.where||""};
+    console.log(json);
     var r = yield db.collection('inserts').insertOne(json);
     assert.equal(1, r.insertedCount);
     // Close connection
@@ -66,6 +69,18 @@ app.get('/setdb', function(request, response) {
   }).catch(function(err) {
     console.log(err.stack);
   });
+
+});
+
+app.get('/cleardb', function(request, response) {
+  // Connection URL
+    var url = 'mongodb://heroku_9c85x4j1:v4fqtsggd87bq3ntfbjnnopp9a@ds145667.mlab.com:45667/heroku_9c85x4j1';
+    // Use connect method to connect to the server
+    MongoClient.connect(url, function(err, db) {
+      db.collection('inserts').remove(); 
+      response.send("Clear Collection `inserts` Done");
+    });
+
 
 });
 
