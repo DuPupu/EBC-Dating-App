2fc55d9f4451fc8bba0c75e93ac6be11b1300d15
DuPupu <zouluoyi@gmail.com>
2016-11-10 18:21:30 +0800

    a

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+  response.send("<script>window.location='pair.html';</script>");
 });
 
 app.get('/getdb', function(request, response) {
