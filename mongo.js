7eb858d8d1bd4b830e2249d99e200539fdba6337
DuPupu <zouluoyi@gmail.com>
2016-11-10 15:19:51 +0800

    A

--- mongo.js
+++ mongo.js
@@ -1,61 +1,19 @@
-var mongodb = require('mongodb');
-var uri = 'mongodb://heroku_9c85x4j1:v4fqtsggd87bq3ntfbjnnopp9a@ds145667.mlab.com:45667/heroku_9c85x4j1';
-mongodb.MongoClient.connect(uri, function(err, db) {
-  
-  if(err) throw err;
-  
-  /*
-   * First we'll add a few songs. Nothing is required to create the 
-   * songs collection; it is created automatically when we insert.
-   */
 
-  var songs = db.collection('songs');
+var MongoClient = require('mongodb').MongoClient,
+  co = require('co'),
+  assert = require('assert');
 
-   // Note that the insert method can take either an array or a dict.
+co(function*() {
+  // Connection URL
+  var db = yield MongoClient.connect('mongodb://heroku_9c85x4j1:v4fqtsggd87bq3ntfbjnnopp9a@ds145667.mlab.com:45667/heroku_9c85x4j1');
+  console.log("Connected correctly to server");
 
-  songs.insert(seedData, function(err, result) {
-    
-    if(err) throw err;
-
-    /*
-     * Then we need to give Boyz II Men credit for their contribution
-     * to the hit "One Sweet Day".
-     */
-
-    songs.update(
-      { song: 'One Sweet Day' }, 
-      { $set: { artist: 'Mariah Carey ft. Boyz II Men' } },
-      function (err, result) {
-        
-        if(err) throw err;
-
-        /*
-         * Finally we run a query which returns all the hits that spend 10 or
-         * more weeks at number 1.
-         */
-
-        songs.find({ weeksAtOne : { $gte: 10 } }).sort({ decade: 1 }).toArray(function (err, docs) {
-
-          if(err) throw err;
-
-          docs.forEach(function (doc) {
-            console.log(
-              'In the ' + doc['decade'] + ', ' + doc['song'] + ' by ' + doc['artist'] + 
-              ' topped the charts for ' + doc['weeksAtOne'] + ' straight weeks.'
-            );
-          });
-         
-          // Since this is an example, we'll clean up after ourselves.
-          songs.drop(function (err) {
-            if(err) throw err;
-           
-            // Only close the connection when your app is terminating.
-            db.close(function (err) {
-              if(err) throw err;
-            });
-          });
-        });
-      }
-    );
-  });
+  // Insert a single document
+  var json = {name:"DKSmith",gender:"m",avCode:"m001",where:"AC1"};
+  var r = yield db.collection('inserts').insertOne(json);
+  assert.equal(1, r.insertedCount);
+  // Close connection
+  db.close();
+}).catch(function(err) {
+  console.log(err.stack);
 });
